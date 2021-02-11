947b0143864037124298f8e8eea3d9434540b29f
Nitiphat2 <moozknot@gmail.com>
2021-02-11 17:21:15 +0700

    เสดเเล้ว

--- 6320502410_lab2_6.c
+++ 6320502410_lab2_6.c
@@ -21,5 +21,25 @@ int main()
         }
         n=n/10;
     }
-    printf("f");
+    for(;i>0;)
+    {
+        i--;
+        switch(o[i])
+        {
+            case 0 : printf("Zero"); break;
+            case 1 : printf("One"); break;
+            case 2 : printf("Two"); break;
+            case 3 : printf("Three"); break;
+            case 4 : printf("Four"); break;
+            case 5 : printf("Five"); break;
+            case 6 : printf("Six"); break;
+            case 7 : printf("Seven"); break;
+            case 8 : printf("Eight"); break;
+            case 9 : printf("Nine"); break;
+        }
+        if(i>0)
+        {
+            printf("-");
+        }
+    }
 }
