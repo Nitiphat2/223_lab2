9f279773ebbd42fdbe42ec7dce81c2a34f0a38d9
Nitiphat2 <moozknot@gmail.com>
2021-02-11 15:52:32 +0700

    5

--- 6320502410_lab2_3.c
+++ 6320502410_lab2_3.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main()
 {
-
+    int A,B,C,num[3],i;
+    for(i = 0;i<3;i++)
+    {
+        scanf("%d",&num[i]);
+    }
+    char na[3];
+     for(i = 0;i<3;i++)
+    {
+        scanf(" %c",&na[i]);
+    }
 }
