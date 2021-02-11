33ee7e6bef820c19819abc6c2cafa748b218400a
Nitiphat2 <moozknot@gmail.com>
2021-02-11 16:49:04 +0700

    ทำนาน
    5555

--- 6320502410_lab2_4.c
+++ 6320502410_lab2_4.c
@@ -1,1 +1,81 @@
-
+#include<stdio.h>
+int main()
+{
+    char a,c[10];
+    scanf("%c",&a);
+    int n,k;
+    scanf("%d",&n);
+    k = n % 2;
+    if(k == 1)
+    {
+        if(a >= 65 && a <= 73)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\(^_^)/");
+            }
+            else
+            {
+                printf("(^_^)");
+            }
+        }
+        else if(a >= 74 && a <= 82)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\(*o*)/");
+            }
+            else
+            {
+                printf("(*o*)");
+            }
+        }
+        else if(a >= 83 && a <= 90)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\(T_T)/");
+            }
+            else
+            {
+                printf("(T_T)");
+            }
+        }
+    }
+    else
+    {
+        if(a >= 65 && a <= 73)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\{@_@}/");
+            }
+            else
+            {
+                printf("{@_@}");
+            }
+        }
+        else if(a >= 74 && a <= 82)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\{*v*}/");
+            }
+            else
+            {
+                printf("{*v*}");
+            }
+        }
+        else if(a >= 83 && a <= 90)
+        {
+            if(n == 5 || n == 15 || n == 25)
+            {
+                printf("\{x_x}/");
+            }
+            else
+            {
+                printf("{x_x}");
+            }
+        }
+    }
+}
