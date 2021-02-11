e59d5fd37f8c48cb66cc1f7ff23efb2fd7a922f2
Nitiphat2 <moozknot@gmail.com>
2021-02-11 15:31:08 +0700

    8

--- 6320502410_lab2_2.c
+++ 6320502410_lab2_2.c
@@ -9,14 +9,14 @@ int main()
     if(c == 'A')
     {
         cost = cost - 200;
-        pro = cost * 2;
+        pro = cost * 3;
         c1 = floor(pro);
         a = pro * 100;
         a = a%120;
         if(a >= 60)
         {
             pro = pro - c1 - 0.60;
-            sum = 199 + c1 + 1 + pro;
+            sum = 199 + c1 + 1;
             printf("%.2f",sum);
         }
         else
@@ -27,6 +27,21 @@ int main()
     }
     else if(c == 'B')
     {
-
+        cost = cost - 400;
+        pro = cost * 2;
+        c1 = floor(pro);
+        a = pro * 100;
+        a = a%120;
+        if(a >= 60)
+        {
+            pro = pro - c1 - 0.60;
+            sum = 299 + c1 + 1 + pro;
+            printf("%.2f",sum);
+        }
+        else
+        {
+            sum = 299 + pro;
+            printf("%.2f",sum);
+        }
     }
 }
