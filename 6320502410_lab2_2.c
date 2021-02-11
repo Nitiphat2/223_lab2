6b3b85d6da7a61c627de888530ecbd4524fcbc28
Nitiphat2 <moozknot@gmail.com>
2021-02-11 15:42:04 +0700

    55

--- 6320502410_lab2_2.c
+++ 6320502410_lab2_2.c
@@ -4,7 +4,7 @@ int main()
     char c;
     scanf("%c",&c);
     float cost,sum=0,pro = 0;
-    int a,c1;
+    int a,c1,i;
     scanf("%f",&cost);
     if(c == 'A')
     {
@@ -12,18 +12,17 @@ int main()
         pro = cost * 3;
         c1 = floor(pro);
         a = pro * 100;
-        a = a%120;
-        if(a >= 59)
+        while(a > 60)
         {
-            pro = pro - c1 - 0.60;
-            sum = 199 + c1 + 1 + pro;
-            printf("%.2f",sum);
+            a = a % 60;
+            i++;
         }
-        else
+        if(i = 1)
         {
-            sum = 199 + pro;
-            printf("%.2f",sum);
+            pro = pro - 0.60;
         }
+        sum = 199 + i + pro;
+        printf("%.2f",sum);
     }
     else if(c == 'B')
     {
@@ -31,17 +30,16 @@ int main()
         pro = cost * 2;
         c1 = floor(pro);
         a = pro * 100;
-        a = a%120;
-        if(a >= 59)
+        while(a > 60)
         {
-            pro = pro - c1 - 0.60;
-            sum = 299 + c1 + 1 + pro;
-            printf("%.2f",sum);
+            a = a % 60;
+            i++;
         }
-        else
+        if(i = 1)
         {
-            sum = 299 + pro;
-            printf("%.2f",sum);
+            pro = pro - 0.60;
         }
+        sum = 299 + i + pro;
+        printf("%.2f",sum);
     }
 }
