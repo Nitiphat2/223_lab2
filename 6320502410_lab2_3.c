970d7b5de476b6d341b7a19b3f96535fb3df63c7
Nitiphat2 <moozknot@gmail.com>
2021-02-11 15:57:18 +0700

    5

--- 6320502410_lab2_3.c
+++ 6320502410_lab2_3.c
@@ -1,14 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int A,B,C,num[3],i;
-    for(i = 0;i<3;i++)
-    {
-        scanf("%d",&num[i]);
-    }
+    int A,B,C,num[3],i,count;
+    scanf("%d%d%d",A,B,C);
     char na[3];
-     for(i = 0;i<3;i++)
+    for(i = 0;i<3;i++)
     {
         scanf(" %c",&na[i]);
     }
+    if(A>B)
+    {
+        count=A;
+        A=B;
+        B=count;
+    }
+    if(A>B)
+    {
+        count=A;
+        A=C;
+        C=count;
+    }
+    if(B>C)
+    {
+        count=B;
+        B=C;
+        C=count;
+    }
+
 }
