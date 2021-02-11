e43bb8ba52b9dab8a2e53549de0fd084b74cab24
Nitiphat2 <moozknot@gmail.com>
2021-02-11 15:18:22 +0700

    6

--- 6320502410_lab2_2.c
+++ 6320502410_lab2_2.c
@@ -4,17 +4,18 @@ int main()
     char c;
     scanf("%c",&c);
     float cost,sum=0,pro = 0;
-    int a;
+    int a,c1;
     scanf("%f",&cost);
     if(c == 'A')
     {
         cost = cost - 200;
         pro = cost * 2;
+        c1 = floor(pro);
         a = pro * 100;
         a = a%120;
         if(a >= 60)
         {
-            sum = 199 + pro + 1;
+            sum = 199 + c1 + 1;
             printf("%.2f",sum);
         }
         else
